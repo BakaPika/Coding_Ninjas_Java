63f66ac810cb836d962860aa2d61548f18992ba3
Sahil Sharma <95086118+BakaPika@users.noreply.github.com>
2023-05-19 09:29:39 +0530

    Update Return Array Sum.java

--- Introduction to Java/ Lecture 9 : Arrays - 1/Return Array Sum.java
+++ Introduction to Java/ Lecture 9 : Arrays - 1/Return Array Sum.java
@@ -11,7 +11,7 @@ public class Solution {
 
     public static int sum(int[] arr)
     {
-		int tSum = 0;
+	int tSum = 0;
         for(int i = 0; i < arr.length; i++)
         {
             tSum = tSum + arr[i];
