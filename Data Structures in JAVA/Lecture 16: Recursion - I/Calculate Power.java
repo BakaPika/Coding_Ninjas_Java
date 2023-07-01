0590080cc1e0f90d715d027b906b8ab2046d9f68
Sahil Sharma <95086118+BakaPika@users.noreply.github.com>
2023-07-01 13:42:45 +0530

    Create Calculate Power.java

---
+++ Data Structures in JAVA/Lecture 16: Recursion - I/Calculate Power.java
@@ -0,0 +1,29 @@
+// Write a program to find x to the power n (i.e. x^n). Take x and n from the user. You need to return the answer.
+// Do this recursively.
+// 3 4
+// Sample Output 1 :
+// 81
+// Sample Input 2 :
+//  2 5
+// Sample Output 2 :
+// 32
+
+public class Solution {
+
+	public static int power(int x, int n) {
+		/* Your class should be named Solution
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * Return output and don't print it.
+		 * Taking input and printing output is handled automatically.
+		 */
+
+		 if(n == 0)
+        {
+            return 1;
+        }
+
+        return x * power(x, n-1);
+		
+	}
+}
